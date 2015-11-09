a7c42f043c5a44bac87f2b138d26864be82596c0
Valentin Rocher <vrocher@webedia.fr>
2015-11-09 08:31:19 +0100

    change map

--- app/src/main/java/org/toulibre/cdl/fragments/MapFragment.java
+++ app/src/main/java/org/toulibre/cdl/fragments/MapFragment.java
@@ -17,44 +17,55 @@ import java.util.Locale;
 
 public class MapFragment extends Fragment {
 
-	private static final double DESTINATION_LATITUDE = 43.6022058;
-	private static final double DESTINATION_LONGITUDE = 1.4530564;
-	private static final String DESTINATION_NAME = "ENSEEIHT";
+    private static final double DESTINATION_LATITUDE = 43.6022058;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setHasOptionsMenu(true);
-	}
+    private static final double DESTINATION_LONGITUDE = 1.4530564;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_map, container, false);
-	}
+    private static final String DESTINATION_NAME = "ENSEEIHT";
 
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		inflater.inflate(R.menu.map, menu);
-	}
+    private static final String NATIVE_URI = "google.navigation:q=%1$f,%2$f";
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.directions:
-				launchDirections();
-				return true;
-		}
-		return false;
-	}
+    private static final String WEB_URI = "http://maps.google.com/maps?f=d&daddr=%1$f,%2$f(%3$s)&dirflg=r";
 
-	private void launchDirections() {
-		// Build intent to start Google Maps directions
-		String uri = String.format(Locale.US,
-				"http://maps.google.com/maps?f=d&daddr=%1$f,%2$f(%3$s)&dirflg=r",
-				DESTINATION_LATITUDE, DESTINATION_LONGITUDE, DESTINATION_NAME);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
-		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
 
-		startActivity(intent);
-	}
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.map, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.directions:
+                launchDirections();
+                return true;
+        }
+        return false;
+    }
+
+    private void launchDirections() {
+        Uri uri;
+        Intent intent;
+
+        uri = Uri.parse(String.format(Locale.US, NATIVE_URI, DESTINATION_LATITUDE, DESTINATION_LONGITUDE));
+        intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setPackage("com.google.android.apps.maps");
+        if (intent.resolveActivity(getActivity().getPackageManager()) == null) {
+            intent.setPackage(null);
+            intent.setData(Uri.parse(String.format(Locale.US, WEB_URI, DESTINATION_LATITUDE,
+                    DESTINATION_LONGITUDE, DESTINATION_NAME)));
+        }
+
+        startActivity(intent);
+    }
 }
