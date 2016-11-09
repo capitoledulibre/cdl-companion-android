72900a669acff7a0c0ecd3105cb2b0610e105c12
Valentin Rocher <valentin.rocher@webedia-group.com>
2016-11-09 08:12:27 +0100

    Fix crash #2

--- app/src/main/java/org/toulibre/cdl/utils/customtabs/CustomTabsHelperFragment.java
+++ app/src/main/java/org/toulibre/cdl/utils/customtabs/CustomTabsHelperFragment.java
@@ -2,6 +2,8 @@ package org.toulibre.cdl.utils.customtabs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
@@ -14,13 +16,20 @@ public class CustomTabsHelperFragment extends Fragment {
 
     private static final String TAG = "CustomTabsHelperFragmen";
 
-    public static void attach(FragmentActivity activity) {
+    private static final Handler sMainHandler = new Handler(Looper.getMainLooper());
+
+    public static void attach(final FragmentActivity activity) {
         if (activity != null
                 && activity.getSupportFragmentManager().findFragmentByTag(TAG) == null) {
-            activity.getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(new CustomTabsHelperFragment(), TAG)
-                    .commit();
+            sMainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    activity.getSupportFragmentManager()
+                            .beginTransaction()
+                            .add(new CustomTabsHelperFragment(), TAG)
+                            .commit();
+                }
+            });
         }
     }
 
