0241b0f76a3a82a9d79674b5dd3909ef9299cc28
François Magimel <magimel.francois@gmail.com>
2018-11-02 16:27:23 +0100

    Update the year of the schedule to 2018

--- app/src/main/java/org/toulibre/capitoledulibre/api/Urls.java
+++ app/src/main/java/org/toulibre/capitoledulibre/api/Urls.java
@@ -7,11 +7,11 @@ package org.toulibre.capitoledulibre.api;
  */
 public class Urls {
 
-    private static final String SCHEDULE_URL = "https://participez-2017.capitoledulibre.org/schedule/xml/";
+    private static final String SCHEDULE_URL = "https://participez-2018.capitoledulibre.org/schedule/xml/";
 
-    //private static final String EVENT_URL_FORMAT = "https://participez-2017.capitoledulibre.org/programme/presentation/%d/";
+    //private static final String EVENT_URL_FORMAT = "https://participez-2018.capitoledulibre.org/programme/presentation/%d/";
 
-    //private static final String PERSON_URL_FORMAT = "https://participez-2017.capitoledulibre.org/speaker/profile/%d/";
+    //private static final String PERSON_URL_FORMAT = "https://participez-2018.capitoledulibre.org/speaker/profile/%d/";
 
     public static String getSchedule() {
         return SCHEDULE_URL;
